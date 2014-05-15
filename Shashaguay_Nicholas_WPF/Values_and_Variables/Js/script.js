f31c67ae0f0c651a5036370d5c1215fa6490ff3c
shiggs22 <shaguay@fullsail.edu>
2014-05-15 19:49:47 -0400

    Nothing to see

--- Shashaguay_Nicholas_WPF/Values_and_Variables/Js/script.js
+++ Shashaguay_Nicholas_WPF/Values_and_Variables/Js/script.js
@@ -1,10 +1,50 @@
 // JaveScript
 
-var yearBorn = 1955;
+// Sparky's age is 7 in dog years
 
-/*
-console.log("Hello World!");
-console.log(yearBorn);
+var dogAge = 1;
+var humanAge = 7;
+
+// Mulitply Dog's age (1) to Humans age (7)
+
+var actualAge = dogAge * humanAge;
+console.log(actualAge);
+
+// Number of of slices per pizza
+
+var slices = 8;
+
+// Number of people at the party
+
+var students = 20;
+
+// Number of pizzas ordered
+
+var pizzas = 6;
+
+// Number of slices in total. Take 8 and multiply by 6 to get total number of slices. Then divid by number of students to get the total result.
+
+/*var total = (slices * pizzas)/ students;
+console.log(total);
 	*/
-	
-alert(yearBorn);
+
+
+
+// Number of of slices per pizza
+
+var slices = 8;
+
+// Number of people at the party
+
+var students = 20;
+
+// Number of pizzas ordered
+
+var pizzas = 6;
+
+// To get number of slices the dog will get you need to take 8 and multiply by number of slices, then divid by the number students, then subtract from 
+
+var total = slices * pizzas/ students;
+console.log(total);
+
+
