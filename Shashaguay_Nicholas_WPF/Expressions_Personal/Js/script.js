a30279b89b251fbb5bdb9dba174ada3638be4fb4
shiggs22 <shaguay@fullsail.edu>
2014-05-15 22:51:14 -0400

    added total amount of game cost var without taxes

--- Shashaguay_Nicholas_WPF/Expressions_Personal/Js/script.js
+++ Shashaguay_Nicholas_WPF/Expressions_Personal/Js/script.js
@@ -3,3 +3,9 @@
 // Xbox 1 games pricing
 
 var gamePrice = [59.99, 169.99, 44.99];
+
+// total amount of pricing without tax
+
+var totalwithoutTax = gamePrice[0] + gamePrice[1] + gamePrice[2];
+console.log(totalwithoutTax); // %274.97
+
