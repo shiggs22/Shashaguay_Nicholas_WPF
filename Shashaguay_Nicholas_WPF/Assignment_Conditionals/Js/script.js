00c7f89d6d7f3aedc79057b9b63d761e7b31e59e
shiggs22 <shaguay@fullsail.edu>
2014-05-22 19:15:36 -0400

    added 2 vars people in gym and girls in gym

--- Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
+++ Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
@@ -1,7 +1,11 @@
 //	Shashaguay, Nicholas
 //	05/22/2014
-//	Assignment: Conditionals
+//	Assignment: Conditionals personal
 
+// This will determine if its a good time to go the gym or not to go
+
+var peopleInGym = 5;
+var girlsInGym = 2;
 
 	
 	
