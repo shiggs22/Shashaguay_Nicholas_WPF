b38a9dd542d87ca7d58f345bc09f3b0a335d48c2
shiggs22 <shaguay@fullsail.edu>
2014-05-22 19:56:52 -0400

    added another var friends in gym then changed girls in gym to cute girls in gym

--- Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
+++ Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
@@ -5,7 +5,18 @@
 // This will determine if its a good time to go the gym or not to go
 
 var peopleInGym = 5;
-var girlsInGym = 2;
+var girlsInGym = true;
+
+// If more than 5 people in the gym, "Don't bother."
+// If girls are in the gym, "Go get your swole on!!!!"
+
+if(peopleInGym >= 5){
+	console.log("Don't Bother.");
+}else if(girlsInGym){
+	console.log("Go get your swole on!!!");
+}else{
+	console.log("Go get your swole on!!!");
+}
 
 	
 	
