edc4e2a9d43c36f75bf6085c1574306842a6e1e0
shiggs22 <shaguay@fullsail.edu>
2014-05-22 20:39:13 -0400

    added console.log for "else if" for if a friend is working out on a bench, you can workout with him.

--- Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
+++ Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
@@ -6,12 +6,13 @@
 
 var benchesInGym = 4;
 var peopleUseingBenches = 5;
-var friendsUsingBench = 1;
+var friendsUsingBench = true;
 
 if(peopleUseingBenches < benchesInGym){
-	//If people using the benches is greater than available benches "Wait until another bench is available"
+	//If people using the benches is greater than available benches "Wait until another bench is available" if not "workout chest!"
 	console.log("Workout chest!");
-}else if(friendsUsingBench < peopleUseingBenches){
+}else if(friendsUsingBench){
+	//If all benches are taken but you have a friend on a bench, "work in with him."
 	console.log("Work in with him.");
 }else{
 	console.log("Find another workout to do.");
