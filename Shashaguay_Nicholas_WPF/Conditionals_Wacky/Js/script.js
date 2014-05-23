31f888c6965abaa45f877a357a9a9961488407a6
shiggs22 <shaguay@fullsail.edu>
2014-05-22 21:06:05 -0400

    did a ternary for if mirrior size is greater than 5 ft to take a gym selfie or not

--- Shashaguay_Nicholas_WPF/Conditionals_Wacky/Js/script.js
+++ Shashaguay_Nicholas_WPF/Conditionals_Wacky/Js/script.js
@@ -1,3 +1,14 @@
 //	Shashaguay, Nicholas
 //	05/22/2014
 //	Assignment: Conditionals wacky
+
+
+// This will determine if it's a good time for a gym selfie or not.
+
+var mirriorSize = 12;
+var image;
+
+// If the mirrior in the gym is 5 ft or bigger. Take a gym selfie!
+
+image = (mirriorSize > 5) ? "Take a gym selfie!" : "Don't bother."
+console.log(image);
