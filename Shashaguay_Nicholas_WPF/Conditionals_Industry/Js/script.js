2af02344a7c9e51ad45afe235f543f8c2b33aef6
shiggs22 <shaguay@fullsail.edu>
2014-05-22 22:27:14 -0400

    added console log for username added prompt for password

--- Shashaguay_Nicholas_WPF/Conditionals_Industry/Js/script.js
+++ Shashaguay_Nicholas_WPF/Conditionals_Industry/Js/script.js
@@ -6,3 +6,12 @@
 //Promts for login into company profile
 
 var userName = prompt("Enter username:");
+if(userName === ""){
+	//user did not put in username
+	userName = prompt("Please fill in your user name:");
+}
+console.log(userName);
+
+
+//Password entry prompt
+var password = prompt("Enter password:");
