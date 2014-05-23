ab7cd02d60b4a48f68d7efb3ae345239c2a89af7
shiggs22 <shaguay@fullsail.edu>
2014-05-22 20:14:09 -0400

    changed all the vars in "personal" to make better sense for this assignment

--- Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
+++ Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
@@ -4,18 +4,12 @@
 
 // This will determine if its a good time to go the gym or not to go
 
-var peopleInGym = 5;
-var friendsInGym = 2;
-var cuteGirlsInGym = 1;
+var benchesInGym = 3;
+var peopleUseingBenches = 2;
+var friendsUsingBench = 1;
 
 // If more than 5 people in the gym, "Don't bother."
-// If girls are in the gym, "Go get your swole on!!!!"
 
-if(peopleInGym >= 5){
-	console.log("Don't Bother.");
-}else{
-	console.log("Go get your swole on!!!");
-}
 
-	
+
 	
