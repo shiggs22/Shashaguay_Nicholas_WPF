0465a904b37fcfe03d1ac6007fab2d938f0ac9db
shiggs22 <shaguay@fullsail.edu>
2014-05-22 20:01:34 -0400

    added "if" condidtion, if 5 people or less are in the "workout"

--- Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
+++ Shashaguay_Nicholas_WPF/Assignment_Conditionals/Js/script.js
@@ -5,15 +5,14 @@
 // This will determine if its a good time to go the gym or not to go
 
 var peopleInGym = 5;
-var girlsInGym = true;
+var friendsInGym = 2;
+var cuteGirlsInGym = 1;
 
 // If more than 5 people in the gym, "Don't bother."
 // If girls are in the gym, "Go get your swole on!!!!"
 
 if(peopleInGym >= 5){
 	console.log("Don't Bother.");
-}else if(girlsInGym){
-	console.log("Go get your swole on!!!");
 }else{
 	console.log("Go get your swole on!!!");
 }
