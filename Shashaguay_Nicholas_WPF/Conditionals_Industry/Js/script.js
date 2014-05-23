df36c9db8779875415181b6d4ba436ae9cb48f46
shiggs22 <shaguay@fullsail.edu>
2014-05-22 22:46:42 -0400

    added var for workload for how many webpages must be done, completed, or not finsihed

--- Shashaguay_Nicholas_WPF/Conditionals_Industry/Js/script.js
+++ Shashaguay_Nicholas_WPF/Conditionals_Industry/Js/script.js
@@ -21,3 +21,17 @@ if(password === ""){
 }
 console.log(password); //password is stored using console.log
 
+
+var webPagesDue = 50;
+var webPagesDone = 48;
+var webPagesTurnedIn = 45;
+
+//50 webpages are due for your boss. If you have completed more than 40 web pages this week you're on track to meet the deadline for this project to be done on time, if not you will have to stay and work during the weekend.
+
+if(webPagesDue > webPagesDone && webPagesDone > 40){
+	//Show this message if the person is on track with their work
+	console.log("You're on track with all your work.");
+}else{
+	//Show this message if the work isn't on track
+	console.log("Work isn't on track. Must come in this weekend.");
+}
