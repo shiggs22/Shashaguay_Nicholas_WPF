f5a1bcd79231c10c704e92fa9b023de37575e305
shiggs22 <shaguay@fullsail.edu>
2014-05-22 22:30:38 -0400

    added console log for if person did not enter a password

--- Shashaguay_Nicholas_WPF/Conditionals_Industry/Js/script.js
+++ Shashaguay_Nicholas_WPF/Conditionals_Industry/Js/script.js
@@ -10,8 +10,14 @@ if(userName === ""){
 	//user did not put in username
 	userName = prompt("Please fill in your user name:");
 }
-console.log(userName);
+console.log(userName); //Username is stored via console.log
 
 
 //Password entry prompt
 var password = prompt("Enter password:");
+if(password === ""){
+	//If user didn't put a password in show another prompt for them to enter password
+	password = prompt("Please fill in your password to continue:");
+}
+console.log(password); //password is stored using console.log
+
