8de510f3fdd51723c2bba99458d777bf26f26b31
shiggs22 <shaguay@fullsail.edu>
2014-05-29 21:21:35 -0400

    created a "while" loop for if the user left the prompt blank or didn't use a number but use a sting instead

--- Shashaguay_Nicholas_WPF/Functions_Personal/Js/script.js
+++ Shashaguay_Nicholas_WPF/Functions_Personal/Js/script.js
@@ -16,3 +16,11 @@ var moneyToSpend = prompt("Please tell us how much money you have to spend on vi
 //Logging in the amount with a console.log
 
 console.log(moneyToSpend);
+
+//Making sure the user is using numbers and not words or leaving the prompt blank
+
+while(isNaN(moneyToSpend) || moneyToSpend === ""){
+	
+	//re-prompting the user if they didn't use a number or just left the prompt blank
+	moneyToSpend = prompt("Please put in a NUMBER and DO NOT leave blank:");
+}
