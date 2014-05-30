750c5501dbdd5c0f3ebde0b57889f9b8f5f151d6
shiggs22 <shaguay@fullsail.edu>
2014-05-29 23:27:50 -0400

    created prompt and re-prompt for men in the gym also for if they didnt put a number or left blank

--- Shashaguay_Nicholas_WPF/Functions_Wacky/Js/script.js
+++ Shashaguay_Nicholas_WPF/Functions_Wacky/Js/script.js
@@ -2,4 +2,15 @@
 //05/29/2014
 //Functions_Wacky
 
-alert("test");
+//alert("test");
+
+//Creating a prompt for the number of men in the gym
+var menInGym = prompt("How many men are in the gym?:");
+//console.log to make sure the number records
+console.log(menInGym);
+
+while(isNaN(menInGym) || menInGym === ""){
+	
+	//re-prompting the user if they didn't use a number or just left the prompt blank
+	menInGym = prompt("Please put in a NUMBER and DO NOT leave blank:");
+}
