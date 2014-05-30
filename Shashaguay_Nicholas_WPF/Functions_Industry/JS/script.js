e12bd693fcad7bbd01a130c169c242cf91a57620
shiggs22 <shaguay@fullsail.edu>
2014-05-29 22:43:47 -0400

    made a re prompt for when the user doesn't put a number or leaves blank

--- Shashaguay_Nicholas_WPF/Functions_Industry/JS/script.js
+++ Shashaguay_Nicholas_WPF/Functions_Industry/JS/script.js
@@ -10,4 +10,10 @@
 var pageAmount = prompt("How much money do you charge for one page of just HTML?:");
 
 //making a console.log just to make sure the number is being recored
-console.log(pageAmount);
+//console.log(pageAmount);
+
+while(isNaN(pageAmount) || pageAmount === ""){
+	
+	//re-prompting the user if they didn't use a number or just left the prompt blank
+	moneyToSpend = prompt("Please put in a NUMBER and DO NOT leave blank:");
+}
