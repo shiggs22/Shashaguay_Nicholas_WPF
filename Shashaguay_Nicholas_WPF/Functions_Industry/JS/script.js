39e49e4f5783cbec9cf0a7b84df0aeb0dfe66dd0
shiggs22 <shaguay@fullsail.edu>
2014-05-29 22:40:33 -0400

    made a prompt for the amount of money charged for a page for only HTMl

--- Shashaguay_Nicholas_WPF/Functions_Industry/JS/script.js
+++ Shashaguay_Nicholas_WPF/Functions_Industry/JS/script.js
@@ -4,3 +4,10 @@
 
 
 //alert("test");
+
+//Creating a prompt for a survey of how much one charges for a website vs other competitors
+
+var pageAmount = prompt("How much money do you charge for one page of just HTML?:");
+
+//making a console.log just to make sure the number is being recored
+console.log(pageAmount);
