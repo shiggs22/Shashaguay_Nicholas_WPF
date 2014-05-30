2cba734852e94d1189359dd8be6eaa1613d410c7
shiggs22 <shaguay@fullsail.edu>
2014-05-29 22:58:30 -0400

    created a prompt for amount of money you charge for designing a website

--- Shashaguay_Nicholas_WPF/Functions_Industry/JS/script.js
+++ Shashaguay_Nicholas_WPF/Functions_Industry/JS/script.js
@@ -10,10 +10,16 @@
 var pageAmount = prompt("How much money do you charge for one page of just HTML?:");
 
 //making a console.log just to make sure the number is being recored
-//console.log(pageAmount);
+console.log(pageAmount);
 
 while(isNaN(pageAmount) || pageAmount === ""){
 	
 	//re-prompting the user if they didn't use a number or just left the prompt blank
-	moneyToSpend = prompt("Please put in a NUMBER and DO NOT leave blank:");
+	pageAmount = prompt("Please put in a NUMBER and DO NOT leave blank:");
 }
+
+//Creating a prompt for how much one charges for designing a page for a website
+
+var pageDesign = prompt("How much money do you charge for designing a page for a website?:");
+//making a console.log to make sure the number us being recorded
+console.log(pageDesign); 
